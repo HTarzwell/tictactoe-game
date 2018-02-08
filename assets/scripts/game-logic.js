21241d5205c8a5c7b22453455fae88d5510ae13e
HTarzwell <htarzwell@gmail.com>
2018-02-08 13:03:48 -0500

    blech

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -9,16 +9,19 @@ const gameBoardSquares = function (event) {
   }
 }
 
-const checkIfNull = function (event) {
-  if (gameBoardSquares.gameBoard[i] !== null) {
-    console.log('please choose another square')
-  } else {
-    console.log('go ahead!')
+const checkIfNull = function (array) {
+  let i = gameBoard[0]
+  if (i !== 'undefined') {
+    console.log
   }
-  return checkIfNull
 }
 
 // problems: 4: writing a function that assigns a value to an empty space in the array and returns a warning if already assigned
+// problems: 4.1: function has to be a function
+// problems: 4.2 function has to take an element of the gameBoard array as a parameter
+// problems: 4.3: function has to determine if element !=== undefined
+// problems: 4.4: function has to return an alert if element !=== undefined
+
 // problems: 4.5: writing a function that selects a specific array slot and checks if it is available (i.e has non-null property)
 // problems: 5: writing a function that varies the element assigned to the array slot depending on user (X and O)
 // problems: 6: writing a function that tracks and determines a win condition (three in a row vertical, horizonal, diagonal)
