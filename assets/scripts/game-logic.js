c26436ef6676b213f500deda756ceed7c93c43d2
HTarzwell <htarzwell@gmail.com>
2018-02-09 09:56:27 -0500

    Added win condition variables and working on function

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -3,13 +3,8 @@
 let gameBoard = new Array(9).fill('') // technique from Stack Overflow
 
 const currentPlayer = {
-  token: 'x',
-  checkToken: function () { // this is a this.example from Nate!
-    return this.token
-  }
+  token: 'x'
 }
-// var currentPlayer
-currentPlayer.checkToken()
 
 const gameBoardSquares = function (event) {
   console.log('Im inside gameBoardSquares and gameBoard is', gameBoard)
@@ -49,14 +44,52 @@ const emptyBoard = function () {
 
 // create emptyBoard function to make gameBoard into an empty array
 
+/** winConditions: token check for following:
+  x === gameBoard[0, 1, 2]
+  x === gameBoard[3, 4, 5]
+  x === gameBoard[6, 7, 8]
+  x === gameBoard[0, 3, 6]
+  x === gameBoard[1, 4, 7]
+  x === gameBoard[2, 5, 8]
+  x === gameBoard[0, 4, 8]
+  x === gameBoard[6, 4, 2]
+**/
+
+const winConditionsForX = [
+  ['x', 'x', 'x', '', '', '', '', '', ''],
+  ['', '', '', 'x', 'x', 'x', '', '', ''],
+  ['', '', '', '', '', '', 'x', 'x', 'x'],
+  ['x', '', '', 'x', '', '', 'x', '', ''],
+  ['', 'x', '', '', 'x', '', '', 'x', ''],
+  ['', '', 'x', '', '', 'x', '', '', 'x'],
+  ['x', '', '', '', 'x', '', '', '', 'x'],
+  ['', '', 'x', '', 'x', '', 'x', '', '']
+]
+
+const winConditionsForO = [
+  ['o', 'o', 'o', '', '', '', '', '', ''],
+  ['', '', '', 'o', 'o', 'o', '', '', ''],
+  ['', '', '', '', '', '', 'o', 'o', 'o'],
+  ['o', '', '', 'o', '', '', 'o', '', ''],
+  ['', 'o', '', '', 'o', '', '', 'o', ''],
+  ['', '', 'o', '', '', 'o', '', '', 'o'],
+  ['o', '', '', '', 'o', '', '', '', 'o'],
+  ['', '', 'o', '', 'o', '', 'o', '', '']
+]
+
+const checkWinConditions = function (array) {
+  if (array === winConditionsForX.winOne) {
+    return 'Player X Wins!'
+  }
+}
+
 // problems: 4: writing a function that assigns a value to an empty space in the array and returns a warning if already assigned
 // problems: 4.1: function has to be a function
 // problems: 4.2 function has to take an element of the gameBoard array as a parameter
 // problems: 4.3: function has to determine if element !=== undefined
 // problems: 4.4: function has to return an alert if element !=== undefined
+// problems: 4.5: function has to prevent makeMove if element !=== undefined
 
-// problems: 4.5: writing a function that selects a specific array slot and checks if it is available (i.e has non-null property)
-// problems: 5: writing a function that varies the element assigned to the array slot depending on user (X and O)
 // problems: 6: writing a function that tracks and determines a win condition (three in a row vertical, horizonal, diagonal)
 
 // export game board
