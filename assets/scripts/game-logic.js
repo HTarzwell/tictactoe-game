92ce2c749060833c3d0695b4f1bca46c5e78167b
HTarzwell <htarzwell@gmail.com>
2018-02-11 16:10:56 -0500

    Messy ugly commit with some working clicks

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -1,35 +1,48 @@
-// first attempt at game board creation
+// first attempt at game logic
+let gameBoard = new Array(9).fill('') // leave it empty and push from DOM?
 
-let gameBoard = new Array(9).fill('') // technique from Stack Overflow
-
-const currentPlayer = {
-  token: 'x'
-}
-
-const gameBoardSquares = function (event) {
-  console.log('Im inside gameBoardSquares and gameBoard is', gameBoard)
-  for (let i = gameBoard[0]; i <= gameBoard.length; i++) {
-    console.log(gameBoard[i])
-  }
-}
-
-// function takes an array position and determines whether it has a value of undefined
-// checkIfUndefined(2)
-
-const makeMove = function (index) {
-  if (gameBoard[index] === '') {
-    gameBoard[index] = currentPlayer.token
-  } else {
-    return 'choose another square'
-  }
-}
-// makeMove puts a value in an array position
-
-const switchPlayer = function (currentPlayer) { // pass in currentPlayer because the variable needs to be worked on
-  if (currentPlayer.token === 'x') {
-    currentPlayer.token = 'o'
-  } else {
-    currentPlayer.token = 'x'
+const gameOn = {
+  playerToken: 'x',
+  gameBoardSquares: function (event) {
+    console.log('Im inside gameBoardSquares and gameBoard is', gameBoard)
+    for (let i = gameBoard[0]; i <= 9; i++) {
+      console.log(gameBoard[i])
+    }
+  },
+  makeMove: function (index) {
+    if (gameBoard[index] === '') { // note: is this what you want or do you need undefined?
+      gameBoard[index] = this.playerToken
+    } else {
+      return 'choose another square'
+    }
+  },
+  switchPlayer: function () {
+    if (this.playerToken === 'x') {
+      this.playerToken = 'o'
+    } else {
+      this.playerToken = 'x'
+    }
+  },
+  winCondition: function () {
+    if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) {
+      return 'Game Winner!'
+    } else if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) {
+      return 'Game Winner!'
+    } else if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) {
+      return 'Game Winner!'
+    } else if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
+      return 'Game Winner!'
+    } else if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
+      return 'Game Winner!'
+    } else if (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) {
+      return 'Game Winner!'
+    } else if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
+      return 'Game Winner!'
+    } else if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
+      return 'Game Winner!'
+    } else {
+      return 'Draw Game'
+    }
   }
 }
 // function switchPlayer
@@ -39,38 +52,12 @@ const emptyBoard = function () {
 }
 
 // create emptyBoard function to make gameBoard into an empty array
-
-const winCondition = function () {
-  if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) {
-    return 'Game Winner!'
-  } else if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) {
-    return 'Game Winner!'
-  } else if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) {
-    return 'Game Winner!'
-  } else if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
-    return 'Game Winner!'
-  } else if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
-    return 'Game Winner!'
-  } else if (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) {
-    return 'Game Winner!'
-  } else if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
-    return 'Game Winner!'
-  } else if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
-    return 'Game Winner!'
-  } else {
-    return 'Draw Game'
-  }
-}
 // problems: 6: writing a function that tracks and determines a win condition (three in a row vertical, horizonal, diagonal)
 // WIN CONDITION IS A BOOLEAN
 // export game board
 
 module.exports = {
   gameBoard,
-  gameBoardSquares,
-  emptyBoard,
-  makeMove,
-  currentPlayer,
-  switchPlayer,
-  winCondition
+  gameOn,
+  emptyBoard
 }
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,6 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
-const gameLogic = require('./game-logic')
 const authEvents = require('./auth/events')
 
 $(() => {
@@ -15,10 +14,92 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+// first attempt at game logic
+let gameBoard = new Array(9).fill('') // #game-board all child elements
+
+let spaceZero = $('#squarezero')
+let spaceOne = $('#squareone') // assign variable to DOM id, .text() onclick to assign X or O to id, then return and push id value to array
+let spaceTwo = $('#squaretwo')
+let spaceThree = $('#squarethree')
+let spaceFour = $('#squarefour')
+let spaceFive = $('#squarefive')
+let spaceSix = $('#squaresix')
+let spaceSeven = $('#squareseven')
+let spaceEight = $('#squareeight')
+
+$('#squarezero').on('click', function () {
+  $('#squarezero').text(gameOn.playerToken)
+})
+
+$('#squareone').on('click', function () {
+  $('#squareone').text(gameOn.playerToken)
+})
+
+const gameOn = {
+  playerToken: 'x',
+  gameBoardSquares: function (event) {
+    console.log('Im inside gameBoardSquares and gameBoard is', gameBoard)
+    for (let i = gameBoard[0]; i <= 9; i++) {
+      console.log(gameBoard[i])
+    }
+  },
+  makeMove: function (index) {
+    if (gameBoard[index] === '') { // note: is this what you want or do you need undefined?
+      gameBoard[index] = this.playerToken
+    } else {
+      return 'choose another square'
+    }
+  },
+  switchPlayer: function () {
+    if (this.playerToken === 'x') {
+      this.playerToken = 'o'
+    } else {
+      this.playerToken = 'x'
+    }
+  },
+  winCondition: function () {
+    if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) {
+      return 'Game Winner!'
+    } else if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) {
+      return 'Game Winner!'
+    } else if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) {
+      return 'Game Winner!'
+    } else if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
+      return 'Game Winner!'
+    } else if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
+      return 'Game Winner!'
+    } else if (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) {
+      return 'Game Winner!'
+    } else if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
+      return 'Game Winner!'
+    } else if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
+      return 'Game Winner!'
+    } else {
+      return 'Draw Game'
+    }
+  }
+}
+// function switchPlayer
+
+const emptyBoard = function () {
+  gameBoard = new Array(9).fill('')
+}
+
+// create emptyBoard function to make gameBoard into an empty array
+// problems: 6: writing a function that tracks and determines a win condition (three in a row vertical, horizonal, diagonal)
+// WIN CONDITION IS A BOOLEAN
+// export game board
+
+$('#game-board').ready(function () {
+  $('#game-board-button').on('click', gameOn.gameBoardSquares)
+})
+
 $(() => {
   authEvents.addHandlers()
 })
 
-$(() => {
-  $('#game-board-button').on('click', gameLogic.gameBoardSquares)
-})
+module.exports = {
+  gameBoard,
+  gameOn,
+  emptyBoard
+}
